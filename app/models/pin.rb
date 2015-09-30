3942c97a38b0bac068c3cbadd9de7e6261328775
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-09-30 20:00:28 +0200

    Add default image on pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>" }
+  has_attached_file :image, styles: { medium: "300x300>" }, default_url: "lucio.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
