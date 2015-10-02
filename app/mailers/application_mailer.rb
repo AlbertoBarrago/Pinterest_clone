91a75b1dc1f55031db6a68fae1e865633ea4a9d0
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-10-03 00:14:43 +0200

    delete mailer

--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "a.barrago69@gmail.com"
-  layout 'mailer'
-end
--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: 'pinterestclonex.com'
-
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Pin Generated with success')
-  end
-end
