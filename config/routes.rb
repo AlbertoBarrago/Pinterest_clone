ab1b7882cab72447a2e360bc65623d8a886c761b
a.barrago69@gmail.com <a.barrago69@gmail.com>
2015-10-03 15:50:36 +0200

    a step back

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount Commontator::Engine => '/commontator'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  devise_for :users
 
   resources :pins do
     member do
